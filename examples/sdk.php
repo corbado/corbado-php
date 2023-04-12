e6f9b1642ab4c0d5fc8e8d72dd5412e884c0b2b2
Corbadoman <100508310+corbadoman@users.noreply.github.com>
2023-04-12 08:20:57 +0200

    #4: Started with JWT validation

--- examples/sdk.php
+++ examples/sdk.php
@@ -2,16 +2,20 @@
 
 use Corbado\Configuration;
 use Corbado\SDK;
+use Symfony\Component\Cache\Adapter\ArrayAdapter;
 
 require_once '../vendor/autoload.php';
 
 try {
+    $jwksCachePool = new ArrayAdapter();
+
     $config = new Configuration();
     $config->setProjectID('pro-1')
-        ->setApiSecret('43jlk5j43lk5j34kl');
+           ->setApiSecret('43jlk5j43lk5j34kl')
+           ->setJwksCachePool($jwksCachePool);
 
     $sdk = new SDK($config);
-    $shortSession = $sdk->shortSession();
+    $valid = $sdk->shortSession()->validate('fldkjgflkdgfdjkl');
 
 } catch (Throwable $e) {
     echo $e->getMessage();
--- src/Classes/ShortSession.php
+++ src/Classes/ShortSession.php
@@ -2,6 +2,8 @@
 
 namespace Corbado\Classes;
 
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 use Psr\Cache\CacheItemPoolInterface;
 
 class ShortSession
@@ -11,6 +13,35 @@ class ShortSession
     public function __construct(CacheItemPoolInterface $jwksCachePool)
     {
         $this->jwksCachePool = $jwksCachePool;
+    }
+
+    /**
+     * @throws \Corbado\Exceptions\Assert
+     */
+    public function validate(string $jwt) : bool
+    {
+        Assert::stringNotEmpty($jwt);
+
+        $key = 'example_key';
+        $payload = [
+            'iss' => 'http://example.org',
+            'aud' => 'http://example.com',
+            'iat' => 1356999524,
+            'nbf' => 1357000000
+        ];
+
+        /**
+         * IMPORTANT:
+         * You must specify supported algorithms for your application. See
+         * https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40
+         * for a list of spec-compliant algorithms.
+         */
+        $jwt = JWT::encode($payload, $key, 'HS256');
+        $decoded = JWT::decode($jwt, new Key($key, 'HS256'));
+
+        print_r($decoded);
+
+        return false;
     }
 
 }
