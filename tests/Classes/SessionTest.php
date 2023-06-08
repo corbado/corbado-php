cf9af15d8aa035de5c2418ac834c69f98a524dfe
Corbadoman <100508310+corbadoman@users.noreply.github.com>
2023-06-08 13:12:58 +0200

    #4: Adding anonym. classes for PSR6 cache so we can remove the symfony dependency

--- tests/Classes/SessionTest.php
+++ tests/Classes/SessionTest.php
@@ -11,6 +11,8 @@ use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\TestCase;
+use Psr\Cache\CacheItemInterface;
+use Psr\Cache\CacheItemPoolInterface;
 use Symfony\Component\Cache\Adapter\ArrayAdapter;
 
 class SessionTest extends TestCase
@@ -127,6 +129,85 @@ class SessionTest extends TestCase
 
         $handlerStack = HandlerStack::create($mock);
         $client = new Client(['handler' => $handlerStack]);
+
+        $cacheItem = new class implements CacheItemInterface {
+            public function getKey(): string
+            {
+                return '';
+            }
+
+            public function get(): mixed
+            {
+                return null;
+            }
+
+            public function isHit(): bool
+            {
+                return false;
+            }
+
+            public function set(mixed $value): static
+            {
+                return new static();
+            }
+
+            public function expiresAt(?\DateTimeInterface $expiration): static
+            {
+                // TODO: Implement expiresAt() method.
+            }
+
+            public function expiresAfter(\DateInterval|int|null $time): static
+            {
+                // TODO: Implement expiresAfter() method.
+            }
+        }
+
+        $cacheItemPool = new class implements CacheItemPoolInterface {
+            public function getItem(string $key): CacheItemInterface
+            {
+                return new $cacheItem();
+            }
+
+            public function getItems(array $keys = []): iterable
+            {
+                // TODO: Implement getItems() method.
+            }
+
+            public function hasItem(string $key): bool
+            {
+                // TODO: Implement hasItem() method.
+            }
+
+            public function clear(): bool
+            {
+                // TODO: Implement clear() method.
+            }
+
+            public function deleteItem(string $key): bool
+            {
+                // TODO: Implement deleteItem() method.
+            }
+
+            public function deleteItems(array $keys): bool
+            {
+                // TODO: Implement deleteItems() method.
+            }
+
+            public function save(CacheItemInterface $item): bool
+            {
+                // TODO: Implement save() method.
+            }
+
+            public function saveDeferred(CacheItemInterface $item): bool
+            {
+                // TODO: Implement saveDeferred() method.
+            }
+
+            public function commit(): bool
+            {
+                // TODO: Implement commit() method.
+            }
+        }
 
         return new Session(
             'v2',
