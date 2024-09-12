1a7d1072bfb38b8af379d6a60d1ce6678bdf2e69
Corbadoman <100508310+corbadoman@users.noreply.github.com>
2024-09-12 13:22:05 +0200

    CS fix

--- src/Exceptions/ValidationException.php
+++ src/Exceptions/ValidationException.php
@@ -4,15 +4,16 @@ namespace Corbado\Exceptions;
 
 class ValidationException extends \Exception
 {
-    const CODE_JWT_GENERAL = 1;
-    const CODE_JWT_ISSUER_MISMATCH = 2;
-    const CODE_JWT_INVALID_DATA = 3;
-    const CODE_JWT_INVALID_SIGNATURE = 4;
-    const CODE_JWT_BEFORE = 5;
-    const CODE_JWT_EXPIRED = 6;
+    public const CODE_JWT_GENERAL = 1;
+    public const CODE_JWT_ISSUER_MISMATCH = 2;
+    public const CODE_JWT_INVALID_DATA = 3;
+    public const CODE_JWT_INVALID_SIGNATURE = 4;
+    public const CODE_JWT_BEFORE = 5;
+    public const CODE_JWT_EXPIRED = 6;
 
 
-    public function __construct(string $message, int $code) {
+    public function __construct(string $message, int $code)
+    {
         parent::__construct($message, $code);
     }
 }
