0dbc4e6cdb4e3031ee9968c07ae5d535d1b71c82
Corbadoman <100508310+corbadoman@users.noreply.github.com>
2024-10-24 17:09:29 +0200

    Update SDK.php
    Bumped version

--- src/SDK.php
+++ src/SDK.php
@@ -24,7 +24,7 @@ class SDK
     private ?UserInterface $users = null;
     private ?IdentifierInterface $identifiers = null;
 
-    public const VERSION = '4.2.0';
+    public const VERSION = '4.3.0';
 
     /**
      * Constructor
