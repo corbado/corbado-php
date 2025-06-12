641ee58e2b06f6fcdb1727b659c13447ec50a718
Dopeamin <mohamed.amine.hamdouni@corbado.com>
2025-06-12 13:56:57 +0200

    Update README and OpenAPI generation script for improved documentation links

--- scripts/generate-openapi.sh
+++ scripts/generate-openapi.sh
@@ -7,7 +7,7 @@ rm -rf .gen
 mkdir -p .gen
 cd .gen
 
-curl -s -O https://apireference.cloud.corbado.io/backendapi-v2/backend_api.yml
+curl -s https://backendapi.cloud.corbado.io/v2/openapi.yaml -o backend_api.yml
 docker pull openapitools/openapi-generator-cli
 docker run -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /local/backend_api.yml -g php -o /local --additional-properties=invokerPackage=Corbado\\Generated
 cp -r lib/* ../../src/Generated
